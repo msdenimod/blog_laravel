bab69ae98020381425b8d0745038f2b6a622e2b0
denimod <msdenimod@yandex.ru>
2021-10-22 18:31:51 +0400

    CRUD постов почти готов

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,91 +3,115 @@
 @section('title', 'Админка | Добавление поста')
 
 @section('content')
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1 class="m-0">Добавление поста</h1>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-12">
+                        <h1 class="m-0">Добавление поста</h1>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <!-- Small boxes (Stat box) -->
-            <div class="row">
-                <div class="col-1">
-                    <a href="{{ route('admin.post.index') }}" class="btn btn-block btn-info btn-sm"><< Назад</a>
-                </div>
-                <div class="col-12">
-                    <form action="{{ route('admin.post.store') }}" method="post" class="mt-4" enctype="multipart/form-data">
-                        @csrf
-                        <div class="form-group w-25">
-                            <input type="text" class="form-control" name="title" placeholder="Название поста"
-                            value="{{ old('title') }}"
-                            >
-                            @error('title')
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-1">
+                        <a href="{{ route('admin.post.index') }}" class="btn btn-block btn-info btn-sm"><< Назад</a>
+                    </div>
+                    <div class="col-12">
+                        <form action="{{ route('admin.post.store') }}" method="post" class="mt-4"
+                              enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group w-25">
+                                <input type="text" class="form-control" name="title" placeholder="Название поста"
+                                       value="{{ old('title') }}"
+                                >
+                                @error('title')
                                 <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <textarea id="summernote" name="content">{{ old('content') }}</textarea>
-                            @error('content')
-                            <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group w-25">
-                            <label>Select</label>
-                            <select name="category_id" class="form-control">
-                                @foreach($categories as $category)
-                                    <option value="{{ $category->id }}"
-                                    {{ $category->id == old('category_id') ? ' selected' : '' }}
-                                    >{{ $category->title }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group w-25">
-                            <label for="exampleInputFile">Добавить превью</label>
-                            <div class="input-group">
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="exampleInputFile" name="preview_image">
-                                    <label class="custom-file-label" for="exampleInputFile">Выберите изображение</label>
-                                </div>
+                                @enderror
                             </div>
-                            @error('preview_image')
-                            <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group w-25">
-                            <label for="exampleInputFile">Добавить главное изображение</label>
-                            <div class="input-group">
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="exampleInputFile" name="main_image">
-                                    <label class="custom-file-label" for="exampleInputFile">Выберите изображение</label>
-                                </div>
+                            <div class="form-group">
+                                <textarea id="summernote" name="content">{{ old('content') }}</textarea>
+                                @error('content')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
-                            @error('main_image')
-                            <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" class="btn btn-primary" value="Добавить">
-                        </div>
-                    </form>
+                            <div class="form-group w-25">
+                                <label>Категории</label>
+                                <select name="category_id" class="form-control">
+                                    <option value="">Выберите категорию</option>
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ $category->id == old('category_id') ? ' selected' : '' }}
+                                        >{{ $category->title }}</option>
+                                    @endforeach
+                                </select>
+                                @error('category_id')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-25">
+                                <label>Теги</label>
+                                <select class="select2" name="tag_ids[]" multiple="multiple"
+                                        data-placeholder="Выберите тэги"
+                                        style="width: 100%;">
+                                    @foreach($tags as $tag)
+                                        <option
+                                            {{ is_array(old('tag_ids')) && in_array($tag->id, old('tag_ids')) ? ' selected' : '' }}
+                                            value="{{ $tag->id }}"
+                                        >
+                                            {{ $tag->title }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group w-25">
+                                <label for="exampleInputFile">Добавить превью</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="exampleInputFile"
+                                               name="preview_image">
+                                        <label class="custom-file-label" for="exampleInputFile">Выберите
+                                            изображение</label>
+                                    </div>
+                                </div>
+                                @error('preview_image')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-25">
+                                <label for="exampleInputFile">Добавить главное изображение</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="exampleInputFile"
+                                               name="main_image">
+                                        <label class="custom-file-label" for="exampleInputFile">Выберите
+                                            изображение</label>
+                                    </div>
+                                </div>
+                                @error('main_image')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Добавить">
+                            </div>
+                        </form>
+                    </div>
+                    <!-- ./col -->
                 </div>
-                <!-- ./col -->
-            </div>
-            <!-- /.row -->
+                <!-- /.row -->
 
-        </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
 @endsection
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -26,16 +26,83 @@
                     <a href="{{ route('admin.post.index') }}" class="btn btn-block btn-info btn-sm"><< Назад</a>
                 </div>
                 <div class="col-12">
-                    <form action="{{ route('admin.post.update', $post->id) }}" method="post" class="w-25 mt-4">
+                    <form action="{{ route('admin.post.update', $post->id) }}" method="post" class="mt-4">
                         @csrf
                         @method('PATCH')
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="title" value="{{ $post->title }}" placeholder="Название поста">
+                        <div class="form-group w-25">
+                            <input type="text" class="form-control" name="title" placeholder="Название поста"
+                                   value="{{ $post->title }}"
+                            >
                             @error('title')
-                                <div class="text-danger">{{ $message }}</div>
+                            <div class="text-danger">{{ $message }}</div>
                             @enderror
                         </div>
-                        <input type="submit" class="btn btn-primary" value="Изменить">
+                        <div class="form-group">
+                            <textarea id="summernote" name="content">{{ $post->content }}</textarea>
+                            @error('content')
+                            <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group w-25">
+                            <label>Категории</label>
+                            <select name="category_id" class="form-control">
+                                <option value="">Выберите категорию</option>
+                                @foreach($categories as $category)
+                                    <option value="{{ $category->id }}"
+                                        {{ $category->id == $post->category_id ? ' selected' : '' }}
+                                    >{{ $category->title }}</option>
+                                @endforeach
+                            </select>
+                            @error('category_id')
+                            <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group w-25">
+                            <label>Теги</label>
+                            <select class="select2" name="tag_ids[]" multiple="multiple"
+                                    data-placeholder="Выберите тэги"
+                                    style="width: 100%;">
+                                @foreach($tags as $tag)
+                                    <option
+                                        {{ is_array(old('tag_ids')) && in_array($tag->id, old('tag_ids')) ? ' selected' : '' }}
+                                        value="{{ $tag->id }}"
+                                    >
+                                        {{ $tag->title }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group w-25">
+                            <label for="exampleInputFile">Добавить превью</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="exampleInputFile"
+                                           name="preview_image">
+                                    <label class="custom-file-label" for="exampleInputFile">Выберите
+                                        изображение</label>
+                                </div>
+                            </div>
+                            @error('preview_image')
+                            <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group w-25">
+                            <label for="exampleInputFile">Добавить главное изображение</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="exampleInputFile"
+                                           name="main_image">
+                                    <label class="custom-file-label" for="exampleInputFile">Выберите
+                                        изображение</label>
+                                </div>
+                            </div>
+                            @error('main_image')
+                            <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Изменить">
+                        </div>
                     </form>
                 </div>
                 <!-- ./col -->
