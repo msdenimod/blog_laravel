5f516dab5c592a1b3b24f0978e29447545cc5c67
Denis Modesov <msdenimod@yandex.ru>
2022-01-12 13:52:19 +0400

    Фронт: Список постов, конкретный пост, комментарии, лайки

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,14 @@ class Comment extends Model
     use HasFactory;
     protected $table = 'comments';
     protected $guarded = false;
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getDateAttribute()
+    {
+        return Carbon::parse($this->created_at);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,9 +13,26 @@ class Post extends Model
 
     protected $table = 'posts';
     protected $guarded = false;
+    protected $withCount = ['likedUsers'];
+    protected $with = ['category'];
 
     public function tags(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
+    }
+
+    public function comments(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function likedUsers(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'post_user_likes', 'post_id', 'user_id');
     }
 }
